6b334fc1e17bc3505f79f62ab7ed9e2005dd8b09
Drake Morrison <Drake.Morrison@ey.com>
2019-04-01 07:43:13 -0500

    directive created and added for dropdowns

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,7 +1,12 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[app-dropdown]'
 })
-export class DropdownDirective implements OnInit {
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('click') toggleOpen(eventData: Event) {
+    this.isOpen = !this.isOpen;
+  }
 }
